f867eb91bea2090b16dceec46a15c56d8880c07f
coacopaco <coacopaco@gmail.com>
2026-04-02 09:38:08 +0200

    Fist working version /premade

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: '/PortfolioEstela/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/PortfolioEstela/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
     },
   },
-});
+}));
